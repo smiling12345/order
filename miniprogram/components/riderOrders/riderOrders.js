961c7f246a144f92284618ec37bd00fd904eb1f4
wujugao <2634752907@qq.com>
2022-04-17 16:07:49 +0800

    4.17

---
+++ miniprogram/components/riderOrders/riderOrders.js
@@ -0,0 +1,38 @@
+// components/riderOrders/riderOrders.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        orderNum : Number
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        count:0
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        addCount(){
+            //定义的max就想data中定义的变量一样，可以使用
+            //通过max来控制count的最大值
+            if( this.data.count>this.properties.max) return;
+            this.setData({
+                count : this.data.count+2
+            })
+            this._show()
+        },
+
+        _show(){
+            wx.showToast({
+              title: 'count:'+this.data.count,
+              icon: 'none'
+            })
+        }
+    }
+})

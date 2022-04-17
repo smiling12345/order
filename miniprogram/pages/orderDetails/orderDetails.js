5b9b78ee3938c4b6b341a9310b48235d2f1594bc
Admin <1030025780@qq.com>
2022-04-17 14:14:45 +0800

    订单详情

--- miniprogram/pages/orderDetails/orderDetails.js
+++ miniprogram/pages/orderDetails/orderDetails.js
@@ -5,7 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
+    img : 'https://tse1-mm.cn.bing.net/th/id/OIP-C.y7Wi-4tbRUbQQtz660l02gHaLH?w=184&h=276&c=7&r=0&o=5&dpr=1.24&pid=1.7',
+    address: '泰山区10栋101',
+    time: '明天10:30~11:00',//送达时间
+    money: '1.5元',  //配送费
+    remark: '',   //订单备注
+    packMony: '0.5元', //打包费
+    discount: '2元', //优惠价格
+    sumPrice: '9.5元' //合计
+  },
 
+  //更新订单备注
+  OrderNotes(e){
+    this.setData({
+      remark : e.detail.value
+    })
+  },
+  shopping: function (options) {
+    wx.navigateTo({
+          url: '../canteen/canteen',//要跳转到的页面路径
+  })  
   },
 
   /**
